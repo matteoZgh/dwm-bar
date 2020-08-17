c26f64eff936bc673005c7fca07a737d90c01ef3
matteo <1434386636@qq.com>
2020-08-17 22:38:07 +0800

    My dwm-bar

---
+++ bar-functions/dwm_networkmanager.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# A dwm_bar function to show the current network connection/SSID, private IP, and public IP using NetworkManager
+# Joe Standring <git@joestandring.com>
+# GNU GPLv3
+
+# Dependencies: NetworkManager, curl
+
+dwm_networkmanager () {
+    CONNAME=$(nmcli -a | grep 'Wired connection' | awk 'NR==1{print $1}')
+    if [ "$CONNAME" = "" ]; then
+        CONNAME=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -c 5-)
+    fi
+
+    PRIVATE=$(nmcli -a | grep 'inet4 192' | awk '{print $2}')
+
+    printf "%s" "$SEP1"
+    if [ "$IDENTIFIER" = "unicode" ]; then
+        printf "🌐 %s" "$PRIVATE"
+    else
+        printf "NET %s %s | %s" "$CONNAME" "$PRIVATE" "$PUBLIC"
+    fi
+    printf "%s\n" "$SEP2"
+}
+
+dwm_networkmanager
---
+++ bar-functions/dwm_weather.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# A dwm_bar function to print the weather from wttr.in
+# Joe Standring <git@joestandring.com>
+# GNU GPLv3
+
+# Dependencies: curl
+
+# Change the value of LOCATION to match your city
+dwm_weather() {
+    LOCATION=Hailin
+
+    printf "%s" "$SEP1"
+    if [ "$IDENTIFIER" = "" ]; then
+        printf "%s" "$(curl -s wttr.in/$LOCATION?format=1)"
+    else
+        printf "%s" "$(curl -s wttr.in/$LOCATION?format=1 | grep -o ".[0-9].*")"
+    fi
+    printf "%s\n" "$SEP2"
+}
